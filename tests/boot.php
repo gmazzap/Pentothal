510199c72ed7c5efeccba2155b78faf623ebf19e
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2015-12-29 18:03:35 +0100

    Prepare repo

---
+++ tests/boot.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the Pentothal package.
+ *
+ * (c) Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$vendor = dirname(__DIR__).'/vendor/';
+
+if ( ! realpath($vendor)) {
+    die('Please install via Composer before running tests.');
+}
+
+require_once $vendor.'autoload.php';
+require_once $vendor.'phpunit/phpunit/src/Framework/Assert/Functions.php';
+
+unset($vendor);
