5ce1d0322970bbba3861ec205bf48b52657fcf92
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-01-10 23:06:39 +0100

    Add penthotal file to load all functions

---
+++ pentothal.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the Pentothal package.
+ *
+ * (c) Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once 'src/utils.php';
+require_once 'src/combine.php';
+require_once 'src/bulk.php';
+require_once 'src/general.php';
+require_once 'src/type.php';
+require_once 'src/size.php';
+require_once 'src/contain.php';
+require_once 'src/properties.php';
+require_once 'src/methods.php';
+require_once 'src/map.php';
