9d2a7ee3b7c2018ba65496437726cb44964cc4bf
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-01-11 06:46:11 +0100

    Move combineMap and poolMap test in composition

--- tests/src/CompositionTest.php
+++ tests/src/CompositionTest.php
@@ -286,4 +286,62 @@ final class CompositionTest extends PHPUnit_Framework_TestCase
             [70, false],
         ];
     }
+
+    /**
+     * @dataProvider combineMapDataProvider
+     * @param array $subject
+     * @param bool  $expected
+     */
+    public function testCombineMap($subject, $expected)
+    {
+        $map = [
+            'name'   => 'is_string',
+            'age'    => 'is_int',
+            'accept' => 'is_bool',
+        ];
+
+        $combine = P\combineMap($map);
+
+        $expected ? assertTrue($combine($subject)) : assertFalse($combine($subject));
+    }
+
+    public function combineMapDataProvider()
+    {
+        return [
+            [['name' => 'Giuseppe', 'age' => 33, 'accept' => true], true],
+            [['name' => 'John', 'age' => 20, 'accept' => 1], false],
+            [['name' => 'Jane', 'age' => 0, 'accept' => false], true],
+            [['full name' => 'John', 'age' => 20, 'accept' => true], false],
+            [true, false],
+        ];
+    }
+
+    /**
+     * @dataProvider poolMapDataProvider
+     * @param array $subject
+     * @param bool  $expected
+     */
+    public function testPoolMap($subject, $expected)
+    {
+        $map = [
+            'name'   => 'is_string',
+            'age'    => 'is_int',
+            'accept' => 'is_bool',
+        ];
+
+        $combine = P\poolMap($map);
+
+        $expected ? assertTrue($combine($subject)) : assertFalse($combine($subject));
+    }
+
+    public function poolMapDataProvider()
+    {
+        return [
+            [['name' => 'Giuseppe', 'age' => 33, 'accept' => true], true],
+            [['name' => 'John', 'age' => 20, 'accept' => 1], true],
+            [['name' => 1, 'age' => false, 'accept' => false], true],
+            [['full name' => 0, 'age' => '', 'accept' => 1], false],
+            [true, false],
+        ];
+    }
 }
