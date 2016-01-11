f6f702acea5d6e5c3e6840978538c00d29ad3b1c
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-01-11 05:02:05 +0100

    Move combine/pool map funcs to composition file

--- src/composition.php
+++ src/composition.php
@@ -106,3 +106,55 @@ function poolFactory(array $args, callable $callable)
         return $callable($item);
     }, $args));
 }
+
+/**
+ * @param callable[] $callbacks
+ * @return \Closure
+ */
+function combineMap(array $callbacks)
+{
+    $callbacks = array_filter($callbacks, 'is_callable');
+    if (empty($callbacks)) {
+        return never();
+    }
+
+    return function ($item) use ($callbacks) {
+        if (! is_array($item) && ! is_object($item)) {
+            return false;
+        }
+
+        foreach ($callbacks as $key => $callback) {
+            if (! variadicCallBoolVal(keyApply($key, $callback), func_get_args())) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+}
+
+/**
+ * @param callable[] $callbacks
+ * @return \Closure
+ */
+function poolMap(array $callbacks)
+{
+    $callbacks = array_filter($callbacks, 'is_callable');
+    if (empty($callbacks)) {
+        return never();
+    }
+
+    return function ($item) use ($callbacks) {
+        if (! is_array($item) && ! is_object($item)) {
+            return false;
+        }
+
+        foreach ($callbacks as $key => $callback) {
+            if (variadicCallBoolVal(keyApply($key, $callback), func_get_args())) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+}
