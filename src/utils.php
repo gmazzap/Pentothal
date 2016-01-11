c8b298d2db37b006e5488c28909e581f5c34223f
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-01-11 04:42:30 +0100

    Drop PHP 5.4 support

--- src/utils.php
+++ src/utils.php
@@ -40,7 +40,7 @@ function variadicCall(callable $callable, array $args = [])
  */
 function variadicCallBoolVal(callable $callable, array $args = [])
 {
-    return (bool) variadicCall($callable, $args);
+    return boolval(variadicCall($callable, $args));
 }
 
 /**
